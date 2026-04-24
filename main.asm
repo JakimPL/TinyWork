187382521b0cbafcb8fc8b6eca5db1dd18d48863
JakimPL <jakimpl@gmail.com>
2026-04-24 18:14:33 +0200

    Prepared: main.asm

---
+++ main.asm
@@ -0,0 +1,47 @@
+    org 0x0100
+
+    %include "framework/asm/fconsts.asm"
+    %include "core/dconsts.asm"
+
+    section .text
+start:
+.set_video_mode:
+    mov al, VIDEO_MODE_13H
+    int BIOS_VIDEO_INTERRUPT
+
+palette:
+    %include "core/palette.asm"
+
+    section .text
+main_loop:
+.push_video_memory_segment:
+    push VIDEO_MEMORY_SEGMENT
+    pop es
+
+    %ifndef NO_VSYNC
+vsync:
+    mov dx, VGA_INPUT_STATUS_REGISTER
+.wait_start:
+    in al, dx
+    test al, VERTICAL_RETRACE_STATUS_BIT
+    jz .wait_start
+    %endif
+
+    section .text
+frame:
+    %include "core/demo.asm"
+
+    section .text
+check_input:
+    in al, KEYBOARD_DATA_PORT
+    dec ax
+    jnz main_loop
+
+    %ifdef RETURN_TO_DOS
+return_to_dos:
+    mov ax, TEXT_MODE_3H
+    int BIOS_VIDEO_INTERRUPT
+    ret
+    %endif
+
+    %include "core/includes.asm"
